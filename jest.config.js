52162e7ce122d51efafcc89691ac61d7af92464e
Mohamed KHEFACHA <khemo@FRAW25-TRINITY.dhcp.idf.intranet>
2025-09-04 10:55:24 +0200

    branch master

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/* eslint-disable */
+/**
+ * @type {import('ts-jest').JestConfigWithTsJest}
+ * Type annotation for Jest config with ts-jest
+ */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
+  preset: "ts-jest", // Use ts-jest preset for TypeScript support
+  testEnvironment: "node", // Set the test environment to Node.js
+  testMatch: ["**/tests/**/*.test.ts"], // Only run test files matching this pattern
 };
